c31a3a22af8cf9e5142a939aaca9eb50476f408b
Jakub Gucwa <jakub.gucwa@outlook.com>
2018-08-27 21:31:03 +0200

    Add project files.

---
+++ NabilApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NabilApp.Startup))]
+namespace NabilApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
